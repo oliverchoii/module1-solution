2776f19d1ca4e9b69f9edc6fe17a84ce1e583860
Oliver <sfchoi4@gmail.com>
2018-09-17 00:12:14 +0800

    New folder

---
+++ app.js
@@ -0,0 +1,31 @@
+(function () {
+'use strict';
+
+angular.module('LunchChecker', [])
+
+.controller('LunchCheckerController', function ($scope) {
+  $scope.input = "";
+
+  $scope.TooMuchorWhat = "";
+
+  $scope.displayTooMuchorWhat = function () {
+    var itemCount = calculateNumberOfItem($scope.input);
+    if(itemCount >= 4){
+        $scope.TooMuchorWhat = "Too Much!";
+    }else{
+        $scope.TooMuchorWhat = "Enjoy!";
+    }
+    console.log("itemCount::" + itemCount);
+  };
+
+
+  function calculateNumberOfItem(string) {
+    var totalStringValue = 0;
+    var partsOfStr = string.split(',');
+    return partsOfStr.length;
+  }
+
+});
+
+
+})();
